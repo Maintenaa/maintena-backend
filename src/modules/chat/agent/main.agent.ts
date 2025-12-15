37cf5bb6c3dd08ec88f531e7df73938272d5c649
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-12-15 22:55:52 +0700

    feat: integrate some tool on main agent

--- src/modules/chat/agent/main.agent.ts
+++ src/modules/chat/agent/main.agent.ts
@@ -1,13 +1,25 @@
 import { ReActAgent } from "llamaindex";
 import { llm } from "../../../core/llm";
 import { getEmployeTool } from "../tools/get-employee.tool";
+import { getPartsTool } from "../tools/get-parts.tool";
+import { getAssetsTool } from "../tools/get-assets.tool";
+import { getLocationsTool } from "../tools/get-locations.tool";
+import { getWorkOrderTool } from "../tools/get-work-order.tool";
+import { getPreventiveMaintenanceTool } from "../tools/get-preventive-maintenance.tool";
 
 export function createMainAgent(props: { company_id: number }) {
   const { company_id } = props;
 
   return new ReActAgent({
     llm,
-    tools: [getEmployeTool],
+    tools: [
+      getEmployeTool,
+      getPartsTool,
+      getAssetsTool,
+      getLocationsTool,
+      getWorkOrderTool,
+      getPreventiveMaintenanceTool,
+    ],
     systemPrompt:
       'Kamu bernama AI Maintena. Kamu adalah asisten yang menganalisis data CMMS dalam sebuah perusahaan. Gunakan tools yang telah disediakan untuk mengambil data yang dibutuhkan. Jika kamu tidak mengetahui jawabannya, Jawab saja dengan "Maaf, saya belum memiliki jawaban dari pertanyaan Anda.". Data perusahaan yang kamu wakili adalah perusahaan dengan company_id = ' +
       company_id +
