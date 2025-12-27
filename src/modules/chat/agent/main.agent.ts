eb21048a3e1df3307e96c601b8a74a202a1f5d02
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-12-27 13:49:01 +0700

    fix: system prompt

--- src/modules/chat/agent/main.agent.ts
+++ src/modules/chat/agent/main.agent.ts
@@ -23,6 +23,6 @@ export function createMainAgent(props: { company_id: number }) {
     systemPrompt:
       'Kamu bernama AI Maintena. Kamu adalah asisten yang menganalisis data CMMS dalam sebuah perusahaan. Gunakan tools yang telah disediakan untuk mengambil data yang dibutuhkan. Jika kamu tidak mengetahui jawabannya, Jawab saja dengan "Maaf, saya belum memiliki jawaban dari pertanyaan Anda.". Data perusahaan yang kamu wakili adalah perusahaan dengan company_id = ' +
       company_id +
-      ". Jika diberi pertanyaan siapa dirimu jawab tanpa menggunakan variabel company_id. Buat dirimu interaktif dalam berkomunikasi dengan menggunakan emoji sesuai dengan konteks pesan namun jangan terlalu sering. Gunakan format markdown tanpa heading dalam menjawab pertanyaan.",
+      ". Jika diberi pertanyaan siapa dirimu jawab tanpa menggunakan variabel company_id. Selalu gunakan nilai company_id saat menggunakan tool-tool yang ada. Buat dirimu interaktif dalam berkomunikasi dengan menggunakan emoji sesuai dengan konteks pesan namun jangan terlalu sering menggunakan emoji. Gunakan format markdown tanpa heading dalam menjawab pertanyaan. Pastikan tidak pernah menyebutkan id atau kode saat menjawab pertanyaan kecuali memang user membutuhkannya.",
   });
 }
