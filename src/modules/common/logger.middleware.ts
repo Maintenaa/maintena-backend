371080e271bc374df133fcfe1eb3bd97dea3577d
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-01-05 22:10:10 +0700

    update: company tool

--- src/modules/common/logger.middleware.ts
+++ src/modules/common/logger.middleware.ts
@@ -2,48 +2,52 @@ import Elysia from "elysia";
 import { logger } from "../../core";
 
 export function LoggerMiddleware() {
-  return new Elysia()
-    .derive(() => {
-      const oldTime = new Date();
-      return {
-        oldTime,
-      };
-    })
-    .onBeforeHandle({ as: "global" }, ({ oldTime }) => {
-      oldTime = new Date();
-    })
-    .onAfterResponse({ as: "global" }, (req) => {
-      const {
-        route,
-        request: { method },
-        set,
-        oldTime,
-      } = req;
+	return new Elysia()
+		.derive(() => {
+			const oldTime = new Date();
+			return {
+				oldTime,
+			};
+		})
+		.onBeforeHandle({ as: "global" }, ({ oldTime }) => {
+			oldTime = new Date();
+		})
+		.onAfterResponse({ as: "global" }, (req) => {
+			const {
+				route,
+				request: { method },
+				set,
+				oldTime,
+			} = req;
 
-      const time = new Date().getTime() - (oldTime || new Date()).getTime();
-      const status = (set.status as number) || 200;
+			if (!route) {
+				return;
+			}
 
-      const methodColor =
-        {
-          GET: "blue",
-          POST: "cyan",
-          PUT: "yellow",
-          DELETE: "red",
-        }[method] || "cyan";
+			const time = new Date().getTime() - (oldTime || new Date()).getTime();
+			const status = (set.status as number) || 200;
 
-      const methodFormated = method[methodColor as any];
-      const routeFormated = route[methodColor as any];
-      const statusFormated =
-        status >= 400 ? status.toString().red : status.toString().cyan;
+			const methodColor =
+				{
+					GET: "blue",
+					POST: "cyan",
+					PUT: "yellow",
+					DELETE: "red",
+				}[method] || "cyan";
 
-      const message = `[${methodFormated}] ${routeFormated} completed in ${time}ms with status ${statusFormated}`;
+			const methodFormated = method[methodColor as any];
+			const routeFormated = route[methodColor as any];
+			const statusFormated =
+				status >= 400 ? status.toString().red : status.toString().cyan;
 
-      if (status >= 500) {
-        logger.error(message);
-      } else if (status >= 400) {
-        logger.warn(message);
-      } else {
-        logger.info(message);
-      }
-    });
+			const message = `[${methodFormated}] ${routeFormated} completed in ${time}ms with status ${statusFormated}`;
+
+			if (status >= 500) {
+				logger.error(message);
+			} else if (status >= 400) {
+				logger.warn(message);
+			} else {
+				logger.info(message);
+			}
+		});
 }
