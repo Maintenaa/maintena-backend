85249f5abe721fd17f0f4380764ffc1a27e3c172
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-01-05 22:09:51 +0700

    feat: add company tool

--- src/modules/chat/agent/main.agent.ts
+++ src/modules/chat/agent/main.agent.ts
@@ -6,23 +6,29 @@ import { getAssetsTool } from "../tools/get-assets.tool";
 import { getLocationsTool } from "../tools/get-locations.tool";
 import { getWorkOrderTool } from "../tools/get-work-order.tool";
 import { getPreventiveMaintenanceTool } from "../tools/get-preventive-maintenance.tool";
+import { getCompanyTool } from "../tools/get-company.tool";
 
 export function createMainAgent(props: { company_id: number }) {
-  const { company_id } = props;
+	const { company_id } = props;
 
-  return new ReActAgent({
-    llm,
-    tools: [
-      getEmployeTool,
-      getPartsTool,
-      getAssetsTool,
-      getLocationsTool,
-      getWorkOrderTool,
-      getPreventiveMaintenanceTool,
-    ],
-    systemPrompt:
-      'Kamu bernama AI Maintena. Kamu adalah asisten yang menganalisis data CMMS dalam sebuah perusahaan. Gunakan tools yang telah disediakan untuk mengambil data yang dibutuhkan. Jika kamu tidak mengetahui jawabannya, Jawab saja dengan "Maaf, saya belum memiliki jawaban dari pertanyaan Anda.". Data perusahaan yang kamu wakili adalah perusahaan dengan company_id = ' +
-      company_id +
-      ". Jika diberi pertanyaan siapa dirimu jawab tanpa menggunakan variabel company_id. Selalu gunakan nilai company_id saat menggunakan tool-tool yang ada. Buat dirimu interaktif dalam berkomunikasi dengan menggunakan emoji sesuai dengan konteks pesan namun jangan terlalu sering menggunakan emoji. Gunakan format markdown tanpa heading dalam menjawab pertanyaan. Pastikan tidak pernah menyebutkan id atau kode saat menjawab pertanyaan kecuali memang user membutuhkannya.",
-  });
+	return new ReActAgent({
+		llm,
+		tools: [
+			getCompanyTool,
+			getEmployeTool,
+			getPartsTool,
+			getAssetsTool,
+			getLocationsTool,
+			getWorkOrderTool,
+			getPreventiveMaintenanceTool,
+		],
+		systemPrompt: `Kamu bernama AI Maintena. Kamu adalah asisten yang menganalisis data CMMS dalam sebuah perusahaan. Kamu harus menjawab tiap pertanyaan berdasarkan ketentuan berikut:
+- Data perusahaan yang kamu wakili adalah perusahaan dengan \`company_id\`=${company_id}. 
+- Gunakan tools yang telah disediakan untuk mengambil data yang dibutuhkan. Selalu gunakan nilai company_id saat menggunakan tool-tool yang ada. 
+- Jika kamu tidak mengetahui jawaban dari pertanyaan, jawab saja dengan "Maaf, saya belum memiliki jawaban dari pertanyaan Anda." dan jangan menjawab tanpa sumber data yang jelas.
+- Buat dirimu interaktif dalam berkomunikasi dengan menggunakan emoji sesuai dengan konteks pesan namun jangan terlalu sering menggunakan emoji. 
+- Pastikan tidak pernah menyebutkan id atau kode saat menjawab pertanyaan kecuali memang user membutuhkannya.
+- Pastikan output yang diberikan menggunakan format markdown.
+`,
+	});
 }
---
+++ src/modules/chat/tools/get-company.tool.ts
@@ -0,0 +1,35 @@
+import { tool } from "llamaindex";
+import z from "zod";
+import { logger } from "../../../core";
+import { getCompany } from "../../company/company.service";
+
+export const getCompanyTool = tool(
+	async ({ company_id }) => {
+		logger.info("Using get company tool");
+
+		const { company } = await getCompany(company_id);
+
+		if (!company) {
+			return "Perusahaan tidak ditemukan";
+		}
+
+		return `## Perusahaan
+Nama Perusahaan: ${company.name}
+Total Karyawan: ${company.employees_count_range.join("-")}
+Email: ${company.email}
+Alamat: ${company.address}
+Pemilik: ${company.owner?.name || "-"}
+`;
+	},
+	{
+		name: "get_company_tool",
+		description: `Tool ini digunakan untuk mengambil data perusahaan. Gunakan hanya jika pertanyaan menanyakan perusahaan.
+
+Parameters:
+- company_id: integer
+`,
+		parameters: z.object({
+			company_id: z.number(),
+		}),
+	},
+);
